0f078dd6edd9891d449823fbd81c1ff8ed62ac72
alindobx <tonyromebx@gmail.com>
2020-07-21 23:10:06 -0400

    first commit A.L

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Nike-Logo-1978-present.jpg';
 import './App.css';
 
 function App() {
@@ -7,9 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>
+            Welcome to React
+        </h2>
+          <img alt="basketball" src="https://pimage.sport-thieme.de/detail-fillscale/wilson-basketball-evolution/268-9119"/>
         <a
           className="App-link"
           href="https://reactjs.org"
